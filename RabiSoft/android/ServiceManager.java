7653dff789db64f8ac596d22f75549faee61d204
Rabbit-U <Rabbit-U>
2013-05-18 14:06:18 +0900

    for Service Manager Pro Ad

--- RabiSoft/android/ServiceManager.java
+++ RabiSoft/android/ServiceManager.java
@@ -1,19 +1,40 @@
 package RabiSoft.android;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.IBinder;
 
 public class ServiceManager {
 
-	public static final String m_keyColumn_Action = "action";
+	static class MyConnection implements ServiceConnection {
+
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			// do nothing.
+		}
+
+		public void onServiceDisconnected(ComponentName name) {
+			// do nothing.
+		}
+		
+	};
+	
+	public static final String m_columnAction = "action";
 	
 	public static final String m_pathContent = "content://RabiSoft.ServiceManager.SettingsProvider";
 	
 	public static final String m_pathIntents = "/Intents";
 	
 	public static final String m_paramAction = "action";
+
+	HashMap<String, ArrayList<MyConnection>> m_connections = new HashMap<String, ArrayList<MyConnection>>();
 
 	public static Cursor getCursor(Context context, String action) {
 
@@ -24,6 +45,41 @@ public class ServiceManager {
 		Cursor cursor = resolver.query(uri, null, null, null, null);
 		return cursor;
 
-	}	
+	}
+	
+	public void connect(Context context, String action_from) {
+	
+   		Cursor cursor = ServiceManager.getCursor(context, action_from);
+   		if( cursor != null ) {
+   			ArrayList<MyConnection> connections = new ArrayList<MyConnection>();
+   			int index_action = cursor.getColumnIndex(ServiceManager.m_columnAction);
+   			boolean loop = cursor.moveToFirst();
+   			while( loop ) {
+   				String action_to = cursor.getString(index_action);
+   				Intent service = new Intent();
+   				service.setAction(action_to);
+   				MyConnection connection = new MyConnection();
+   				boolean b = context.bindService(service, connection, Context.BIND_AUTO_CREATE);
+   				if( b ) {
+   					connections.add(connection);
+   				}
+   				loop = cursor.moveToNext();
+   			}
+   			m_connections.put(action_from, connections);
+   	   		cursor.close();
+   		}
 
+	}
+	
+	public void disconnect(Context context, String action) {
+
+		ArrayList<MyConnection> connections = m_connections.remove(action);
+		if( connections != null ) {
+			for( MyConnection connection : connections ) {
+				context.unbindService(connection);
+			}
+		}
+		
+	}
+	
 }
